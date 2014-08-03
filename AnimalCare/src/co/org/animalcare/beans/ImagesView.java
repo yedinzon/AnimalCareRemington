f82bd5fa480337326b976dc7d6f91d89159dba02
Edwin Cardona Valencia <himare771@hotmail.com>
2014-08-02 23:41:47 -0500

    Creación y actualización de páginas de la vista. Carpetas, header, footer, página inicio.

---
+++ AnimalCare/src/co/org/animalcare/beans/ImagesView.java
@@ -0,0 +1,24 @@
+package co.org.animalcare.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ImagesView {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 3; i++) {
+            images.add("New" + i + ".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
